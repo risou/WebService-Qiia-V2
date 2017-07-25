466e2097a01b2eb2cb0e793f50a42895e6952a05
risou <risou.f@gmail.com>
2017-07-25 19:15:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Qiita::V2 - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Qiita::V2;
+
+# DESCRIPTION
+
+WebService::Qiita::V2 is ...
+
+# LICENSE
+
+Copyright (C) risou.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+risou <risou.f@gmail.com>
---
+++ lib/WebService/Qiita/V2.pm
@@ -0,0 +1,39 @@
+package WebService::Qiita::V2;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Qiita::V2 - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Qiita::V2;
+
+=head1 DESCRIPTION
+
+WebService::Qiita::V2 is ...
+
+=head1 LICENSE
+
+Copyright (C) risou.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+risou E<lt>risou.f@gmail.comE<gt>
+
+=cut
+
